af948057d5a4b9878337ecefa59aa7a29d4cd8bd
תום <Tom@twm-mq.local>
2020-09-05 23:51:00 +0300

    Updates

---
+++ precache-manifest.a31bd0fb698d456b206f9be5db5f7c7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5a949effd465531944541d9ae6399f",
+    "url": "/israel-tax/index.html"
+  },
+  {
+    "revision": "18ebfdcef3375fc729b8",
+    "url": "/israel-tax/static/css/main.4bd61a6e.chunk.css"
+  },
+  {
+    "revision": "56662c643e0d994d0805",
+    "url": "/israel-tax/static/js/2.d06994b5.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/israel-tax/static/js/2.d06994b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18ebfdcef3375fc729b8",
+    "url": "/israel-tax/static/js/main.3eaaebb3.chunk.js"
+  },
+  {
+    "revision": "1a248455dd5b062ab888",
+    "url": "/israel-tax/static/js/runtime-main.d95603d5.js"
+  },
+  {
+    "revision": "2ee1553fb2f68765a1d86e0f64687fc1",
+    "url": "/israel-tax/static/media/icon-caret-down.2ee1553f.svg"
+  },
+  {
+    "revision": "d339988a60d92463501e310d7f8864fb",
+    "url": "/israel-tax/static/media/shekel-banknotes.d339988a.jpg"
+  }
+]);
